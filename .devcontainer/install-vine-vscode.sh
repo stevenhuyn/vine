3cca08e8029f29c9b81297ba579cccd7d10ae59f
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2025-10-03 14:03:39 +0000

    Get lsp installation script almost there

---
+++ .devcontainer/install-vine-vscode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+code --install-extension ./lsp/client/dist/vine-vscode.vsix
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-# set -e
 
-# echo "Running post-create setup..."
-# echo "Current user: $(whoami)"
-# echo "Current UID: $(id -u)"
-
-# # Fix ownership of the workspace
-# echo "Fixing workspace permissions..."
-# sudo chown -R ubuntu:ubuntu /workspaces
-
-# # Clean any root-owned build artifacts
-# if [ -d "/workspaces/vine/target" ]; then
-#     echo "Fixing target directory permissions..."
-#     sudo chown -R ubuntu:ubuntu /workspaces/vine/target
-# fi
-
-# echo "Post-create setup complete!"
+# Compiling the VSCode extension
+cd lsp/client
+npm i
+npx vsce package --skip-license --allow-missing-repository --out ./dist/vine-vscode.vsix
